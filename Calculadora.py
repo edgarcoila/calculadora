db2d3d38beb3d10436d24b55939df18e68365edf
Edgar Coila <edcopa@gmail.com>
2025-05-29 22:19:48 -0500

    versión 3.1

--- Calculadora.py
+++ Calculadora.py
@@ -2,14 +2,14 @@ import streamlit as st
 
 st.set_page_config(page_title="Calculadora de Actividad Reumatológica", layout="centered")
 
-from pages import Artritis_reumatoide, Lupus_eritematoso
+from pages import artritis_reumatoide, lupus_eritematoso
 
 st.title("🧮 Calculadora de Actividad Reumatológica")
 
 st.write("Seleccione una opción:")
 
 if st.button("Artritis Reumatoide"):
-    st.switch_page("pages/Artritis_reumatoide.py")
+    st.switch_page("pages/artritis_reumatoide.py")
 
 if st.button("Lupus Eritematoso Sistémico"):
-    st.switch_page("pages/Lupus_eritematoso.py")
+    st.switch_page("pages/lupus_eritematoso.py")
